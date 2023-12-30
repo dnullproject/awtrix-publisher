24cbd605a86c20d39995b975829d750e1233816d
Bohdan Sukhomlinov <shellshock1953@gmail.com>
2023-12-30 15:32:30 +0200

    MQTT client - python-mqtt-topic-uuid

--- dnull_mqtt/mqtt.py
+++ dnull_mqtt/mqtt.py
@@ -2,6 +2,7 @@ from paho.mqtt import client as mqtt_client
 from dnull_mqtt.config import Config
 import json
 from dnull_mqtt.base_log import log
+from uuid import uuid4
 
 
 class MQTT:
@@ -18,7 +19,9 @@ class MQTT:
             else:
                 log.error(f"Failed to connect {self.topic}, return code %d\n", rc)
 
-        client = mqtt_client.Client(f"python-mqtt-{self.topic}")
+        client_id = f"python-mqtt-{self.topic}-{str(uuid4())[:8]}"
+        client = mqtt_client.Client(client_id)
+        log.debug(f"mqtt client created: {client_id}")
         client.username_pw_set(self.config.mqtt_username, self.config.mqtt_password)
         client.on_connect = on_connect
         client.connect(self.config.mqtt_broker, self.config.mqtt_port)
